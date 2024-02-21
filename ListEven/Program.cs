3e645f0d47f3b6ff8b9aeb9c9a306692204c95bb
Cvetina7777 <tt70207252@edu1.pgzaimov.com>
2024-02-21 09:42:57 +0200

    Da

--- ListEven/Program.cs
+++ ListEven/Program.cs
@@ -6,14 +6,35 @@
         {
             List<int> list = Console.ReadLine().Split().Select(int.Parse).ToList();
             List<int> listEven = new List<int>();
+            List<int> listOdd = new List<int>();
             for (int i = 0; i < list.Count; i++)
             {
-                if (list[i]%2 == 0)
+                if (list[i] % 2 == 0)
+                {
+                    listEven.Add(list[i]);
+                }
+                else
+                {
+                    listOdd.Add(list[i]);
+                }
+            }
+            Console.WriteLine("Chetni " + string.Join(" ", listEven));
+            Console.WriteLine("Nechetni" + string.Join("", listOdd));
+
+
+        }
+        static List<int> GetEventElements(List<int> list)
+        {
+            List<int> listOdd = new List<int>();
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (list[i] % 2 == 0)
                 {
                     listEven.Add(list[i]);
                 }
             }
-            Console.WriteLine(string.Join(" ",listEven));
+            return listOdd;
+
         }
     }
 }
